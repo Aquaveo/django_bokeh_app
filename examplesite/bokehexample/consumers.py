ee2d17af14645d84fc938f84519b1bbb9e77c4d1
Michael Souffront <masouffront@gmail.com>
2019-06-27 16:27:10 -0600

    Add comments to consumer.

--- examplesite/bokehexample/consumers.py
+++ examplesite/bokehexample/consumers.py
@@ -6,6 +6,9 @@ log = logging.getLogger(__name__)
 
 
 class CustomBokehAppWebsocketConsumer(BokehAppWebsocketConsumer):
+    """
+    This is meant to illustrate that the Bokeh consumer could be overridden to add custom behavior.
+    """
 
     async def connect(self):
         log.info("CONNECTED")
