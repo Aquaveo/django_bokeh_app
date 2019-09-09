66b00e6f9d38e5e685cdd215ffb8415b75df4324
msouff <msouffront@aquaveo.com>
2019-09-09 15:45:41 -0600

    url with variable example

--- examplesite/examplesite/routing.py
+++ examplesite/examplesite/routing.py
@@ -16,12 +16,16 @@ from bokeh.server.django import autoload
 bokeh_app = autoload("bokehexample", views.shape_viewer_handler)
 kwargs = dict(app_context=bokeh_app.app_context)
 
+bokeh_app_with_variable = autoload(r'^bokehexample/<str:id>/autoload.js$', views.shape_viewer_handler)
+kwargs_with_variable = dict(app_context=bokeh_app_with_variable.app_context)
+
 ws_urls = [url(r'^bokehexample/ws$', WSConsumer, kwargs=kwargs),
-           url(r'^bokehexample/<str:id>/ws$', WSConsumer, kwargs=kwargs)]
+           url(r'^bokehexample/<str:id>/ws$', WSConsumer, kwargs=kwargs_with_variable)]
 
 http_urls = [url(r'^bokehexample/autoload.js$', AutoloadJsConsumer, kwargs=kwargs),
-             url(r'^bokehexample/<str:id>/autoload.js$', AutoloadJsConsumer, kwargs=kwargs),url(r'', AsgiHandler)]
-breakpoint()
+             url(r'^bokehexample/<str:id>/autoload.js$', AutoloadJsConsumer, kwargs=kwargs_with_variable),
+             url(r'', AsgiHandler)]
+
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
     'websocket': AuthMiddlewareStack(
